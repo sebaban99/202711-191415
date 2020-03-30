4b99916b126f31c0b55ad7b27e9dbe32cb94fd21
alejandrol29 <31321710+alejandrol29@users.noreply.github.com>
2020-03-30 20:29:14 -0300

    First commit
    Added projects:
    Domain
    BuisinessLogic
    WebApi
    DataAccess

---
+++ IMMRequest.BusinessLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IMMRequest.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ IMMRequest.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IMMRequest.Domain
+{
+    public class Class1
+    {
+    }
+}
