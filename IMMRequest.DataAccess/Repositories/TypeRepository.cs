b7eeb5d00d3c13319dce66fd59ef6ca6e47554a1
Sebastián Bañales <tatantus1@gmail.com>
2020-04-08 11:36:31 -0300

    [GREEN] Get Type from DB

---
+++ IMMRequest.DataAccess/Repositories/TypeRepository.cs
@@ -0,0 +1,34 @@
+﻿using IMMRequest.Domain;
+using System;
+using System.Data.Common;
+using System.Linq;
+using Type = IMMRequest.Domain.Type;
+
+namespace IMMRequest.DataAccess
+{
+    public class TypeRepository : BaseRepository<Type>
+    {
+        public TypeRepository(IMMRequestContext context)
+        {
+            Context = context;
+        }
+
+        public override Type Get(Guid id)
+        {
+            try
+            {
+                return Context.Types.First(x => x.Id == id);
+            }
+            catch (System.InvalidOperationException)
+            {
+                return null;
+            }
+            catch (DbException)
+            {
+                throw new DataAccessException("Error: could not get specific Entity");
+            }
+        }
+
+
+    }
+}
