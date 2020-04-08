f921d379f98fcff60b29bfd92d836bcf8d20f480
Sebastián Bañales <tatantus1@gmail.com>
2020-04-08 11:59:17 -0300

    [GREEN] Get AdditionalField from DB

---
+++ IMMRequest.DataAccess/Repositories/AdditionalFieldRepository.cs
@@ -0,0 +1,31 @@
+﻿using IMMRequest.Domain;
+using System;
+using System.Data.Common;
+using System.Linq;
+
+namespace IMMRequest.DataAccess
+{
+    public class AdditionalFieldRepository : BaseRepository<AdditionalField>
+    {
+        public AdditionalFieldRepository(IMMRequestContext context)
+        {
+            Context = context;
+        }
+
+        public override AdditionalField Get(Guid id)
+        {
+            try
+            {
+                return Context.AdditionalFields.First(x => x.Id == id);
+            }
+            catch (System.InvalidOperationException)
+            {
+                return null;
+            }
+            catch (DbException)
+            {
+                throw new DataAccessException("Error: could not get specific Entity");
+            }
+        }
+    }
+}
