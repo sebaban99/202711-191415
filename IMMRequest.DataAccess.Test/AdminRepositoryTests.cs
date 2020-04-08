a016d24487dd168a005dcd3be1a8bff024a4e9b2
Sebastián Bañales <tatantus1@gmail.com>
2020-04-08 11:02:36 -0300

    [RED] Get Admin from DB

---
+++ IMMRequest.DataAccess.Test/AdminRepositoryTests.cs
@@ -0,0 +1,54 @@
+﻿using IMMRequest.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.DataAccess.Tests
+{
+    [TestClass]
+    public class AdminRepositoryTests
+    {
+        Admin sebaAdmin = new Admin()
+        {
+            Email = "seba@asd.com",
+            Password = "Pass",
+            Name = "Sebastian",
+            Id = Guid.NewGuid()
+        };
+
+        Admin marcosAdmin = new Admin()
+        {
+            Email = "marcos@asd.com",
+            Password = "Pass",
+            Name = "marcos",
+            Id = Guid.NewGuid()
+        };
+
+        private AdminRepository adminRepositoryInMemory = new AdminRepository(
+            ContextFactory.GetNewContext(ContextType.MEMORY));
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            adminRepositoryInMemory.Empty();
+        }
+
+        [TestMethod]
+        public void GetAdmin_InexistenAdmin_ShouldReturnNull()
+        {
+            Admin requestById = adminRepositoryInMemory.Get(sebaAdmin.Id);
+            Assert.AreEqual(null, requestById);
+        }
+
+        [TestMethod]
+        public void GetAdmin_ExistentAdmin_ShouldReturnSpecificAdminFromDB()
+        {
+
+            adminRepositoryInMemory.Add(sebaAdmin);
+            adminRepositoryInMemory.Add(marcosAdmin);
+
+            Assert.AreEqual(adminRepositoryInMemory.Get(sebaAdmin.Id), sebaAdmin);
+        }
+    }
+}
