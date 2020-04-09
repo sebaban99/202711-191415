ef004a36493e5077419f4afb2bacc768f123e572
Sebastián Bañales <tatantus1@gmail.com>
2020-04-09 01:40:24 -0300

    [GREEN] Get Range from DB

---
+++ IMMRequest.DataAccess/Repositories/RangeRepository.cs
@@ -0,0 +1,31 @@
+﻿using IMMRequest.Domain;
+using System;
+using System.Data.Common;
+using System.Linq;
+
+namespace IMMRequest.DataAccess.Repositories
+{
+    public class RangeRepository : BaseRepository<Range>
+    {
+        public RangeRepository(IMMRequestContext context)
+        {
+            Context = context;
+        }
+
+        public override Range Get(Guid id)
+        {
+            try
+            {
+                return Context.RangeValues.First(x => x.Id == id);
+            }
+            catch (System.InvalidOperationException)
+            {
+                return null;
+            }
+            catch (DbException)
+            {
+                throw new DataAccessException("Error: could not get specific Entity");
+            }
+        }
+    }
+}
