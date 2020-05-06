0188870326b1624458554f5eb7f34a27ff46e150
Sebastián Bañales <tatantus1@gmail.com>
2020-05-06 17:06:49 -0300

    Validator parent class

---
+++ IMMRequest.BusinessLogic/Validator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.BusinessLogic
+{
+    public abstract class Validator<T>
+    {
+        public abstract void ValidateAdd(T entity);
+
+        public abstract void ValidateDelete(T entity);
+
+        public abstract void ValidateUpdate(T entity);
+    }
+}
