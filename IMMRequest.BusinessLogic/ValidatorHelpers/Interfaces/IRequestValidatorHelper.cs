fb30df0722f7815750c462c195f3db224f284a21
Sebastián Bañales <tatantus1@gmail.com>
2020-05-07 12:07:23 -0300

    [REFACTOR] Request validation now in specific validator class

---
+++ IMMRequest.BusinessLogic/ValidatorHelpers/Interfaces/IRequestValidatorHelper.cs
@@ -0,0 +1,16 @@
+﻿using IMMRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.BusinessLogic
+{
+    public interface IRequestValidatorHelper
+    {
+        void ValidateRequestObject(Request request);
+        void ValidateAdd(Request request);
+        void ValidateUpdate(Request request, Request Request);
+        void ValidateType(Request request);
+        void ValidateAFValues(Request request);
+    }
+}
