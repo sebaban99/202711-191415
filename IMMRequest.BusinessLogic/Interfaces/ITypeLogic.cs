e99413f68478157ec3095d1905103a90573b3657
Sebastián Bañales <tatantus1@gmail.com>
2020-05-07 12:51:43 -0300

    [REFACTOR] ITypeLogic new interface

---
+++ IMMRequest.BusinessLogic/Interfaces/ITypeLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Type = IMMRequest.Domain.Type;
+
+namespace IMMRequest.BusinessLogic
+{
+    public interface ITypeLogic
+    {
+        Type Create(Type type);
+        void Remove(Type type);
+    }
+}
