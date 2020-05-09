a2609de033e4aab67403e15fe5bf391971bf1a8f
Sebastián Bañales <tatantus1@gmail.com>
2020-05-09 16:15:32 -0300

    [GREEN] Get all Requests

---
+++ IMMRequest.WebApi/Controllers/RequestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IMMRequest.BusinessLogic;
+using IMMRequest.DataAccess;
+using System;
+using System.Collections.Generic;
+using IMMRequest.Domain;
+
+namespace IMMRequest.WebApi
+{
+    [Route("api/[controller]")]
+    public class RequestController : ControllerBase
+    {
+        private IRequestLogic requestLogic;
+
+        public RequestController(IRequestLogic requestLogic)
+        {
+            this.requestLogic = requestLogic;
+        }
+
+        [AuthenticationFilter()]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Request> requestsInBD = requestLogic.GetAll();
+            List<RequestDTO> requestToReturn = new List<RequestDTO>();
+            foreach (Request req in requestsInBD)
+            {
+                RequestDTO reqDTO = new RequestDTO(req);
+                requestToReturn.Add(reqDTO);
+            }
+            return Ok(requestToReturn);
+        }
+    }
+}
