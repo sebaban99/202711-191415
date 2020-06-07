1e89beecca8beea7ebae2f4f3691b02db7643e6a
Sebastián Bañales <tatantus1@gmail.com>
2020-06-07 03:04:08 -0300

    [RED] Generate reports

---
+++ IMMRequest.Domain/ReportTypeBElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IMMRequest.Domain
+{
+    public class ReportTypeBElement : ReportElement
+    {
+        public Type Type { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                ReportTypeBElement r = (ReportTypeBElement)obj;
+                return this.Type == r.Type && this.Amount == r.Amount;
+            }
+        }
+    }
+}
