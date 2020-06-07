ddc3ad42947fe5bc65c1532d4c6d004143a84448
Sebastián Bañales <tatantus1@gmail.com>
2020-06-07 12:22:12 -0300

    AreaLogic interface and implementation

---
+++ IMMRequest.BusinessLogic.Interface/IAreaLogic.cs
@@ -0,0 +1,12 @@
+﻿using IMMRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.BusinessLogic.Interfaces
+{
+    public interface IAreaLogic
+    {
+        Area Create(Area area);
+    }
+}
