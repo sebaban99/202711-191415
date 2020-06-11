bff53890c7445fcbe35237141516df7a200d4d68
Sebastián Bañales <tatantus1@gmail.com>
2020-06-11 20:27:39 -0300

    Importation logic interface defined

---
+++ IMMRequest.BusinessLogic.Interface/IImportationLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMMRequest.Domain;
+using IMMRequest.Importer;
+
+namespace IMMRequest.BusinessLogic.Interfaces
+{
+    public interface IImportationLogic
+    {
+        List<IImporter> GetImportationsMethods(string path);
+        List<AreaImpModel> ImportFile(IImporter importerSelected, string path);
+        IImporter GetImporter(string name);
+    }
+}
