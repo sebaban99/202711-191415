c807d141e964376c0c04232f5a749ac4c7f65201
Sebastián Bañales <tatantus1@gmail.com>
2020-06-11 20:30:15 -0300

    Created new project Importer with interface for importer implementations

---
+++ IMMRequest.Importer/ImpModels/AreaImpModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.Importer
+{
+    public class AreaImpModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<TopicImpModel> Topics { get; set; }
+
+        public AreaImpModel()
+        {
+            Topics = new List<TopicImpModel>();
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                AreaImpModel a = (AreaImpModel)obj;
+                return this.Name == a.Name;
+            }
+        }
+    }
+}
---
+++ IMMRequest.Importer/ImpModels/TopicImpModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.Importer
+{
+    public class TopicImpModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public AreaImpModel Area { get; set; }
+        public List<TypeImpModel> Types { get; set; }
+
+        public TopicImpModel()
+        {
+            Types = new List<TypeImpModel>();
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                TopicImpModel t = (TopicImpModel)obj;
+                return this.Name == t.Name && this.Area.Equals(t.Area);
+            }
+        }
+    }
+}
